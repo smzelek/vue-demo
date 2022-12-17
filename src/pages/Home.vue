8c3b439b28192b520ed8a279e7aeee734a642ece
Steve Zelek <11831150+smzelek@users.noreply.github.com>
2022-12-17 11:55:04 -0800

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1>This is a home page</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Pilots.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is a page about pilots</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
